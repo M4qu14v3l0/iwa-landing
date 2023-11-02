5f9f98e962480e37c209f14c5a7a325783993f2d
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-11-02 10:48:40 -0500

    party town added again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import svelte from "@astrojs/svelte";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: vercel(),
-  integrations: [
-    svelte(),
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ]
+  integrations: [svelte(), partytown()]
 });
